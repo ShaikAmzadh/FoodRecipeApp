dbcc8b0a2f41c10eb0347020a17612447c00a778
Shaik Amzadh <shaikamzadh425@gmail.com>
2024-07-04 16:06:28 +0530

    Creating data models for individual item screen

---
+++ app/src/main/java/com/example/foodrecipeapp2/models/ProductMatch.kt
@@ -0,0 +1,13 @@
+package com.example.foodrecipeapp2.models
+
+class ProductMatch {
+    var id: Int = 0
+    var title: String? = null
+    var description: String? = null
+    var price: String? = null
+    var imageUrl: String? = null
+    var averageRating: Double = 0.0
+    var ratingCount: Double = 0.0
+    var score: Double = 0.0
+    var link: String? = null
+}
---
+++ app/src/main/java/com/example/foodrecipeapp2/models/WinePairing.kt
@@ -0,0 +1,7 @@
+package com.example.foodrecipeapp2.models
+
+class WinePairing {
+    var pairedWines: ArrayList<String>? = null
+    var pairingText: String? = null
+    var productMatches: ArrayList<ProductMatch>? = null
+}

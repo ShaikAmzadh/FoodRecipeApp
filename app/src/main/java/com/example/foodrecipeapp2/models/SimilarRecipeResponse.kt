b638521c7f57ce00834b4316bcd9fe61ed9649ae
Shaik Amzadh <shaikamzadh425@gmail.com>
2024-07-08 16:08:02 +0530

    SimilarItems fragment--creating model classes

---
+++ app/src/main/java/com/example/foodrecipeapp2/models/SimilarRecipeResponse.kt
@@ -0,0 +1,10 @@
+package com.example.foodrecipeapp2.models
+
+class SimilarRecipeResponse {
+    var id: Int = 0
+    var title: String? = null
+    var imageType: String? = null
+    var readyInMinutes: Int = 0
+    var servings: Int = 0
+    var sourceUrl: String? = null
+}
